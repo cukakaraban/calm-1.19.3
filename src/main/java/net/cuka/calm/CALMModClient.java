9289665d2235ccfa808001b3c2a6900902d0a93f
Cukakaraban <ultimatejonynake@gmail.com>
2023-02-02 20:51:18 +0200

    Initial commit

---
+++ src/main/java/net/cuka/calm/CALMModClient.java
@@ -0,0 +1,10 @@
+package net.cuka.calm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CALMModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
